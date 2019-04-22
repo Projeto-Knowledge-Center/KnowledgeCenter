805f8aed41537a9088f87c11031488c19499b9d4
leandro94costa <leancosta@deloitte.com>
2019-04-22 16:50:49 -0300

    Sprint 03 - 22/04/19 #402025 Criação do mapeamento das rotas que serão utilizadas no portal.

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(req, res, next) {
 // Users
 
 // GET Users
-// Return a JSON array object with all users
+// Return a JSON array object with all Users
 router.get('/users', (req, res) => {
 	testDB.getUsers((error, users) => {
 		if (error) {
@@ -24,7 +24,7 @@ router.get('/users', (req, res) => {
 });
 
 // GET User
-// Return a JSON user object
+// Return a JSON User object for a given username
 router.get('/users/:username', (req, res) => {
 	testDB.getUser(req.params.username, (error, user) => {
 		if (error) {
@@ -45,20 +45,36 @@ router.put('/users/:username', (req, res) => {
 });
 
 // POST Sign Up
+// Require a JSON User object, insert it into the DB and return a JSON response object from the DB
 router.post('/users/signup', (req, res) => {
-
+	const b = req.body;
+	testDB.insertUser(b.email, b.password, (error, response) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.status(200);
+		res.send(response);
+	});
 });
 
 // POST Login
+// Require a JSON User object, return HTTP status 200 and JSON User object if authorized or status 401 for unauthorized
 router.post('/users/login', (req, res) => {
-
+	const b = req.body;
+	testDB.login(b.email, b.password, (error, user) => {
+		if (error) {
+			return res.send(error);
+		}
+		user ? res.status(200) : res.status(401);
+		res.send(user);
+	});
 });
 
 
 // Tags
 
 // GET Tags
-// Return a JSON array object with all tags
+// Return a JSON array object with all Tags
 router.get('/tags', (req, res) => {
 	testDB.getTags((error, tags) => {
 		if (error) {
@@ -72,7 +88,7 @@ router.get('/tags', (req, res) => {
 // Questions
 
 // GET Questions
-// Return a JSON array object with all questions
+// Return a JSON array object with all Questions
 router.get('/questions', (req, res) => {
 	testDB.getQuestions((error, questions) => {
 		if (error) {
@@ -83,13 +99,19 @@ router.get('/questions', (req, res) => {
 });
 
 // GET Question
+// Return a JSON Question object for a given question id
 router.get('/questions/:idQuestion', (req, res) => {
-	
+	testDB.getQuestion(req.params.idQuestion, (error, question) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.send(question);
+	});
 });
 
 // POST Question
 router.post('/questions', (req, res) => {
-
+	
 });
 
 // PUT Question
@@ -103,21 +125,39 @@ router.delete('/questions/:idQuestion', (req, res) => {
 });
 
 // GET tagged Questions
+// Return a JSON array of Question objects for a given Tag
 router.get('/questions/tagged/:tag', (req, res) => {
-
+	testDB.getQuestionsByTag(req.params.tag, (error, questions) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.send(questions);
+	});
 });
 
 
 // Articles
 
 // GET Articles
+// Return a JSON array of Article objects
 router.get('/articles', (req, res) => {
-
+	testDB.getArticles((error, articles) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.send(articles);
+	});
 });
 
 // GET Article
+// Return a JSON Article object for a given id
 router.get('/articles/:idArticle', (req, res) => {
-
+	testDB.getArticle(req.params.idArticle, (error, article) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.send(article);
+	});
 });
 
 // POST Article
@@ -135,9 +175,15 @@ router.delete('/articles/:idArticle', (req, res) => {
 
 });
 
-// GET tagged Article
+// GET tagged Articles
+// Return a JSON array of Article objects for a given Tag
 router.get('/articles/tagged/:tag', (req, res) => {
-
+	testDB.getArticlesByTag(req.params.tag, (error, articles) => {
+		if (error) {
+			return res.send(error);
+		}
+		res.send(articles);
+	});
 });
 
 module.exports = router;
--- routes/testDB.js
+++ routes/testDB.js
@@ -13,7 +13,37 @@ const getUsers = callback => {
 }
 
 const getUser = (username, callback) => {
-    connection.query('SELECT * FROM user u INNER JOIN profile p ON u.id = p.id WHERE p.username = ?', username, (error, results) => {
+    connection.query('SELECT * FROM user u INNER JOIN profile p ON u.id = p.id WHERE p.username = ?', username, (error, result) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(result);
+        }
+    });
+}
+
+const insertUser = (email, password, callback) => {
+    connection.query('INSERT INTO user (email, password, administrator) VALUES (?, ?, 0)', [email, password], (error, response) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(response);
+        }
+    });
+}
+
+const login = (email, password, callback) => {
+    connection.query('SELECT * FROM user WHERE email = ? AND password = ?', [email, password], (error, response) => {
+        if (error) {
+            callback(error);
+        } else {
+            response.length > 0 ? callback(response) : callback();
+        }
+    });
+}
+
+const getTags = callback => {
+    connection.query('SELECT * FROM tag', (error, results) => {
         if (error) {
             callback(error);
         } else {
@@ -22,17 +52,7 @@ const getUser = (username, callback) => {
     });
 }
 
-const getTags = (callback => {
-    connection.query('SELECT * FROM tag', (error, results) => {
-        if (error) {
-            callback(error);
-        } else {
-            callback(results);
-        }
-    });
-});
-
-const getQuestions = (callback => {
+const getQuestions = callback => {
     connection.query('SELECT * FROM question', (error, results) => {
         if (error) {
             callback(error);
@@ -40,11 +60,70 @@ const getQuestions = (callback => {
             callback(results);
         }
     });
-});
+}
+
+const getQuestion = (idQuestion, callback) => {
+    connection.query('SELECT * FROM question q WHERE q.id = ?', idQuestion, (error, result) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(result);
+        }
+    });
+}
+
+const getQuestionsByTag = (tag, callback) => {
+    connection.query('SELECT * FROM question q WHERE q.id = ANY (SELECT question_id FROM question_tag qt WHERE qt.tag_id = ' + 
+                        '(SELECT t.id FROM tag t WHERE t.name = ?))', tag, (error, results) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(results);
+        }
+    });
+}
+
+const getArticles = callback => {
+    connection.query('SELECT * FROM article', (error, results) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(results);
+        }
+    });
+}
+
+const getArticle = (idArticle, callback) => {
+    connection.query('SELECT * FROM article a WHERE a.id = ?', idArticle, (error, result) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(result);
+        }
+    });
+}
+
+const getArticlesByTag = (tag, callback) => {
+    connection.query('SELECT * FROM article a WHERE a.id = ANY (SELECT article_id FROM article_tag at WHERE at.tag_id = ' + 
+                        '(SELECT t.id FROM tag t WHERE t.name = ?))', tag, (error, results) => {
+        if (error) {
+            callback(error);
+        } else {
+            callback(results);
+        }
+    });
+}
 
 module.exports = {
-    getUsers: getUsers,
-    getUser: getUser,
-    getTags: getTags,
-    getQuestions: getQuestions
-};
+    getUsers,
+    getUser,
+    insertUser,
+    login,
+    getTags,
+    getQuestions,
+    getQuestion,
+    getQuestionsByTag,
+    getArticles,
+    getArticle,
+    getArticlesByTag
+}
